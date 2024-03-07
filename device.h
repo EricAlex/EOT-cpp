efe5ca2977b99864cbeef4dfbd1ca2173bf67e50
XinWang <xin.wang@cn.innovusion.com>
2024-03-07 10:40:03 +0800

    remove CUDA related settings and files

--- device.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <thrust/host_vector.h>
-#include <vector>
-
-// function prototype
-void sort_on_device(thrust::host_vector<int>& V);
-double dev_sum(std::vector<double>& V, double init);
